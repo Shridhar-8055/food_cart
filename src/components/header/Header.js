76fbefa6543badd1e015d722435c93b7b4e76811
Shridhar-8055 <teradalshridhar@gmail.com>
2025-02-22 17:26:45 +0530

    Add files via upload

---
+++ src/components/header/Header.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import style from './Header.module.css';
+import { CiSearch } from "react-icons/ci";
+import useWindowSize from '../../constants/useWindowSize';
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css'
+import { PiHamburgerFill } from "react-icons/pi";
+import background from '../../assets/background.jpeg'
+import { useNavigate } from 'react-router-dom';
+
+const Header = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const screenSize = useWindowSize();
+    const mobileView = screenSize <= 600;
+    const navigate = useNavigate();
+
+    const handleClose = () => {
+        setIsDrawerOpen(false);
+    }
+    return (
+        <div className={style.navbar}>
+            <span className={style.brand}>Foodima</span>
+            {mobileView &&
+                <PiHamburgerFill id={style.hamburger} onClick={() => setIsDrawerOpen(true)} />
+            }
+            {!mobileView &&
+                <div className={style.navcontent}>
+                    <div className={style.navlinks}>
+                        <a href='#'>Shop</a>
+                        <a href='#'>Features</a>
+                        <a href='#'>Recipes</a>
+                        <a href='#'>Hotline</a>
+                        <a href='#'><CiSearch /></a>
+                    </div>
+                    <div className={style.profile}>
+                        <span onClick={() => navigate('/')} id={style.btn}>{0}</span>
+                        <span onClick={() => navigate('/login')} id={style.login}>Log in</span>
+                        <span onClick={() => navigate('/signup')} id={style.btn}>Sign Up</span>
+                    </div>
+                </div>
+            }
+            {mobileView && <Drawer open={isDrawerOpen} onClose={handleClose} direction='right' className={style.drawer}>
+                <img src={background} alt="background" className={style.drawerimage} />
+                <div className={style.navcontent}>
+                    <div className={style.navlinks}>
+                        <a href='#'>Shop</a>
+                        <a href='#'>Features</a>
+                        <a href='#'>Recipes</a>
+                        <a href='#'>Hotline</a>
+                        <a href='#'><CiSearch /></a>
+                        <span onClick={() => navigate('/')} id={style.btn}>{0}</span>
+                        <span onClick={() => navigate('/login')} id={style.btn}>Log in</span>
+                        <span onClick={() => navigate('/signup')} id={style.btn}>Sign Up</span>
+                    </div>
+                </div>
+            </Drawer>}
+        </div>
+    )
+}
+
+export default Header
